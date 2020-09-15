a09e6734acda7f9a01c3efdd9c82efb306dd7a96
Shashikanth Addagatla <shashikanth.addagatla@t-mobile.com>
2020-09-15 12:17:05 -0400

    feat: Instant search added

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
@@ -35,6 +36,9 @@ export class BookSearchComponent implements OnInit, OnDestroy {
     this.bookSubscription = this.store.select(getAllBooks).subscribe(books => {
       this.books = books;
     });
+    this.searchForm.get('term').valueChanges.pipe(debounceTime(500), distinctUntilChanged()).subscribe(response => {
+      this.searchBooks();
+    })
   }
 
   formatDate(date: void | string) {
